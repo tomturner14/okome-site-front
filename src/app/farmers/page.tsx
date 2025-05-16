cb41ca4f690ca599713d62e01b99c33a5a28537a
tomturner14 <tomfrom1994@gmail.com>
2025-05-16 22:15:32 +0900

    変数wo _variables.scss に統一

--- src/app/farmers/page.tsx
+++ src/app/farmers/page.tsx
@@ -1,16 +1,17 @@
+'use client';
 import Link from 'next/link';
 import styles from './FarmersPage.module.scss';
 
 export default function FarmersPage() {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.page}>
       <h1>農家紹介ページ</h1>
-      <p>
+      <p className={styles.description}>
         ここでは、おこめ販売で取り扱っている農家さんをご紹介します。
         どの農家さんも安全で美味しいお米作りに情熱を注いでいます。
       </p>
-      <div className={styles.linkArea}>
-        <Link href="/" className={styles.nextButton}>トップへ戻る</Link>
+      <div className={styles.actions}>
+        <Link href="/" className={styles.backButton}>トップへ戻る</Link>
       </div>
     </div>
   );
