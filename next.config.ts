e584046dcad780695744ccac57c646c6e477f642
tomturner14 <tomfrom1994@gmail.com>
2025-05-18 20:44:16 +0900

    next.config.tsにsassOptions追加

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-// next.config.ts の場合
-import type { NextConfig } from 'next'
+// next.config.ts
+import path from 'path';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src')], // ←これを追加
+  },
   async rewrites() {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
     return [
@@ -10,9 +14,8 @@ const nextConfig: NextConfig = {
         source: '/api/:path*',
         destination: `${apiUrl}/:path*`
       }
-    ]
+    ];
   },
-  // domains の代わりに remotePatterns を使用
   images: {
     remotePatterns: [
       {
@@ -22,6 +25,6 @@ const nextConfig: NextConfig = {
       },
     ],
   }
-}
+};
 
-export default nextConfig
+export default nextConfig;
